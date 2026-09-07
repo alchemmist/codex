5bf6d3d97979e33cbdd8869ac4bdabb1f3dab505
alchemmist <anton.ingrish@gmail.com>
2026-09-07 13:33:23 +0300

    reduce custom terminal below module limit

--- antex-rs/tui/src/custom_terminal.rs
+++ antex-rs/tui/src/custom_terminal.rs
@@ -1,26 +1,3 @@
-// This is derived from `ratatui::Terminal`, which is licensed under the following terms:
-//
-// The MIT License (MIT)
-// Copyright (c) 2016-2022 Florian Dehau
-// Copyright (c) 2023-2025 The Ratatui Developers
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
 use std::io;
 use std::io::Write;
 
@@ -462,14 +439,6 @@ where
     /// Restores the user-configured terminal cursor style.
     pub fn reset_cursor_style(&mut self) -> io::Result<()> {
         self.set_cursor_style(SetCursorStyle::DefaultUserShape)
-    }
-
-    /// Gets the current cursor position.
-    ///
-    /// This is the position of the cursor after the last draw call.
-    #[allow(dead_code)]
-    pub fn get_cursor_position(&mut self) -> io::Result<Position> {
-        self.backend.get_cursor_position()
     }
 
     /// Sets the cursor position.
